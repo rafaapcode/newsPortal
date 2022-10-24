5df6f8fec1f8eb2959c17ab42590b65890de01fd
rafaapcode <rafaap2003@gmail.com>
2022-10-24 10:34:27 -0300

    Header - OK

---
+++ news_app/src/Header/Acronymn.js
@@ -0,0 +1,12 @@
+export default new Map([
+    ["Portuguese", "pt"],
+    ["English", "en"],
+    ["Arabic", "ar"],
+    ["Spanish", "es"],
+    ["Dutch", "nl"],
+    ["German", "de"],
+    ["Italian", "it"],
+    ["French", "fr"],
+    ["Russian", "ru"],
+    ["Chinese", "zh"],
+]);
--- news_app/src/Header/Header.jsx
+++ news_app/src/Header/Header.jsx
@@ -1,26 +1,42 @@
 import React, { useMemo, useState } from 'react';
 import { motion } from 'framer-motion';
 import { Input, Button, Dropdown } from '@nextui-org/react';
+import Acronymn from './Acronymn';
 
 export default function Header() {
 
-    const [selected, setSelected] = useState(new Set(["text"]));
+    const [selected, setSelected] = useState(new Set(["Portuguese"]));
+    const [input, setInput] = useState('');
 
     const selectedValue = useMemo(
         () => Array.from(selected).join(", ").replaceAll("_", " "),
         [selected]
     );
 
+    const language = Acronymn.get(selectedValue);
+
+    function getInputValue(event) {
+        const value = event.target.value;
+
+        setInput(value);
+    }
+
+    function click() {
+        console.log(input, language);
+
+    }
+
     return (
         <motion.div className='bg-slate-400 flex w-3/4 h-3/4 items-center justify-center text-center mt-8 rounded-md shadow-xl'>
+
             <motion.div className='w-2/5 ml-5'>
                 <Input
+                    onChange={getInputValue}
                     bordered
                     labelPlaceholder="Search for a news"
                     width='100%'
                     color="default" />
             </motion.div>
-
 
             <motion.div className='ml-10'>
                 <Dropdown>
@@ -35,17 +51,22 @@ export default function Header() {
                         selectedKeys={selected}
                         onSelectionChange={setSelected}
                     >
-                        <Dropdown.Item key="text">Text</Dropdown.Item>
-                        <Dropdown.Item key="number">Number</Dropdown.Item>
-                        <Dropdown.Item key="date">Date</Dropdown.Item>
-                        <Dropdown.Item key="single_date">Single Date</Dropdown.Item>
-                        <Dropdown.Item key="iteration">Iteration</Dropdown.Item>
+                        <Dropdown.Item key="Portuguese">Portuguese</Dropdown.Item>
+                        <Dropdown.Item key="English">English</Dropdown.Item>
+                        <Dropdown.Item key="Arabic">Arabic</Dropdown.Item>
+                        <Dropdown.Item key="Spanish">Spanish</Dropdown.Item>
+                        <Dropdown.Item key="Dutch">Dutch</Dropdown.Item>
+                        <Dropdown.Item key="German">German</Dropdown.Item>
+                        <Dropdown.Item key="Italian">Italian</Dropdown.Item>
+                        <Dropdown.Item key="French">French</Dropdown.Item>
+                        <Dropdown.Item key="Russian">Russian</Dropdown.Item>
+                        <Dropdown.Item key="Chinese">Chinese</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
             </motion.div>
 
             <motion.div className='ml-10'>
-                <Button auto>Search</Button>
+                <Button onPress={click} auto>Search</Button>
             </motion.div>
 
         </motion.div>
