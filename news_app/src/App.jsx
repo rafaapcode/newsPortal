3e42cf1e12bc4fd695a76512b5a72c60bdbe6c1a
rafaapcode <rafaap2003@gmail.com>
2022-10-24 17:27:35 -0300

    Color

--- news_app/src/App.jsx
+++ news_app/src/App.jsx
@@ -3,7 +3,7 @@ import Main from './Main';
 
 function App() {
   return (
-    <div className='w-full h-screen bg-slate-500 flex flex-col items-center'>
+    <div className='w-full h-screen flex flex-col items-center'>
       <Main></Main>
     </div>
   );
--- news_app/src/Cards/Card.jsx
+++ news_app/src/Cards/Card.jsx
@@ -8,7 +8,7 @@ export default function Cards(props) {
     const [news] = useContext(newsContext);
 
     return (
-        <div className='w-full h-screen bg-slate-500 flex mt-10 justify-evenly gap-5 flex-wrap'>
+        <div className='w-full h-screen flex mt-10 justify-evenly gap-5 flex-wrap'>
             {news.map((noticias, index) => {
                 return (<motion.div key={index} className='font-sans rounded-xl w-2/5 h-2/5'>
                     <CardsLayout title={noticias.title} author={noticias.author} date={noticias.publishedAt} url={noticias.url} img={noticias.urlToImage}></CardsLayout>
