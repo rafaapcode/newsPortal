908ef1ecce0d7c5da07370e489e3c16c8f58d211
rafaapcode <rafaap2003@gmail.com>
2022-10-26 18:36:37 -0300

    Sidebar background

--- news_app/src/Sidebar/Sidebar.jsx
+++ news_app/src/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SideBar(){
     return (
-        <div className='bg-white/70 flex flex-col rounded-l-xl shadow-2xl backdrop-blur h-full w-full absolute z-10'>
+        <div className='bg-white/20 flex flex-col rounded-l-xl shadow-2xl backdrop-blur h-full w-full absolute z-10'>
             <h1>teste</h1>
         </div>
     )
