4ca8946171826e5a1d33980d9c914caf1aac6c6a
rafaapcode <rafaap2003@gmail.com>
2022-10-27 20:45:41 -0300

    Removing databse

--- newsApi/src/routes.js
+++ newsApi/src/routes.js
@@ -1,13 +1,8 @@
 import Router from 'express';
 import News from './controller/News';
-import FavoriteNews from './controller/FavoriteNews';
 
 const newRouter = new Router();
 
 newRouter.get('/', News.getNews);
-newRouter.post('/', FavoriteNews.createNews);
-newRouter.get('/favoriteAll', FavoriteNews.getAllNews);
-newRouter.delete('/:id', FavoriteNews.deleteNews);
-
 
 export default newRouter;
