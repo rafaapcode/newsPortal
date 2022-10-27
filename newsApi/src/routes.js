511b3a2f1f64bc7e20766a2bee93027bc634373c
rafaapcode <rafaap2003@gmail.com>
2022-10-27 17:19:48 -0300

    Creating endpoints

--- newsApi/src/routes.js
+++ newsApi/src/routes.js
@@ -4,5 +4,9 @@ import News from './controller/News';
 const newRouter = new Router();
 
 newRouter.get('/', News.getNews);
+newRouter.post('/favoritenews', News.getNews);
+newRouter.get('/favoriteAll', News.getNews);
+newRouter.delete('/:id', News.getNews);
+
 
 export default newRouter;
